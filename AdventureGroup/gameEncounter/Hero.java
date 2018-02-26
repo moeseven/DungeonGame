db13bdefc7d5fbb65a35de9261edcaa430c843d6
moeseven <36843905+moeseven@users.noreply.github.com>
2018-02-26 12:44:24 +0100

    Upadated basic inventory functionality
    hand1, hand2, armor

--- AdventureGroup/gameEncounter/Hero.java
+++ AdventureGroup/gameEncounter/Hero.java
@@ -17,5 +17,81 @@ public class Hero {
 	private int blockBonus;
 	private int damageBonus;
 	private int maxHp;
+	public Hero(){
+		
+	}
+	public Backpack getBackpack() {
+		return backpack;
+	}
+	public void setBackpack(Backpack backpack) {
+		this.backpack = backpack;
+	}
+	public Inventory getInventory() {
+		return inventory;
+	}
+	public void setInventory(Inventory inventory) {
+		this.inventory = inventory;
+	}
+	public Deck getDeck() {
+		return deck;
+	}
+	public void setDeck(Deck deck) {
+		this.deck = deck;
+	}
+	public int getHp() {
+		return hp;
+	}
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	public int getMana() {
+		return mana;
+	}
+	public void setMana(int mana) {
+		this.mana = mana;
+	}
+	public int getBlock() {
+		return block;
+	}
+	public void setBlock(int block) {
+		this.block = block;
+	}
+	public int getTurnDraw() {
+		return turnDraw;
+	}
+	public void setTurnDraw(int turnDraw) {
+		this.turnDraw = turnDraw;
+	}
+	public int getTurnMana() {
+		return turnMana;
+	}
+	public void setTurnMana(int turnMana) {
+		this.turnMana = turnMana;
+	}
+	public int getTurnBlock() {
+		return turnBlock;
+	}
+	public void setTurnBlock(int turnBlock) {
+		this.turnBlock = turnBlock;
+	}
+	public int getBlockBonus() {
+		return blockBonus;
+	}
+	public void setBlockBonus(int blockBonus) {
+		this.blockBonus = blockBonus;
+	}
+	public int getDamageBonus() {
+		return damageBonus;
+	}
+	public void setDamageBonus(int damageBonus) {
+		this.damageBonus = damageBonus;
+	}
+	public int getMaxHp() {
+		return maxHp;
+	}
+	public void setMaxHp(int maxHp) {
+		this.maxHp = maxHp;
+	}
+	
 	
 }
--- AdventureGroup/gameEncounter/Item.java
+++ AdventureGroup/gameEncounter/Item.java
@@ -1,5 +1,14 @@
 package gameEncounter;
 
 public class Item {
-
+	private int weight;
+	private int price;
+	private int category;
+	//1: Hand1  //2: Hand2  //3: BiHand //4: Armor
+	public void mod(Hero hero) {
+		//apply modifiers granated by the item
+	}
+	public void demod(Hero hero) {
+		//remove modifiers granted by the item
+	}
 }
